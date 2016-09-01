256f4475ddacbbe6ea2e97b09d443884a6b748ec
jon-a-nygaard <jon.arild@highsoft.com>
2016-09-01 15:33:49 +0200

    Uninstalled redundant library body-parser.

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 'use strict';
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8085;
 const router = express.Router();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 router.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');   
