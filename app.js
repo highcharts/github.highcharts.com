b84f83350d9e37e06d2da6f5e287bd2ab048606d
jon-a-nygaard <jon.arild@highsoft.com>
2016-10-11 12:38:34 +0200

    Changed default port from 8085 to 80.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = require('./routes/router.js');
 const app = express();
-const port = process.env.PORT || 8085;
+const port = process.env.PORT || 80;
 
 app.use('/', router); // Register router
 app.listen(port); // Start server
