68f3cb5daf865fe8472966d940a1d747bc513b7a
jon-a-nygaard <jon.arild@highsoft.com>
2016-10-12 13:34:53 +0200

    Use simple-git to setup a local Highcharts repository on the server.

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 'use strict';
-const express = require('express');
-const router = require('./routes/router.js');
-const app = express();
-const port = process.env.PORT || 80;
 
-app.use('/', router); // Register router
-app.listen(port); // Start server
+const initRepository = () => {
+	const U = require('./helper/utilities.js');
+	const workingDir = 'github';
+	U.createDirectory(workingDir);
+	const repository = 'https://github.com/highcharts/highcharts.git';
+	const simpleGit = require('simple-git')(__dirname + '/' + workingDir);
+	return simpleGit
+		.init()
+		.then(() => {
+			U.writeFile(workingDir + '/.git/info/sparse-checkout', '/js/');
+		})
+		.addConfig('core.sparsecheckout', 'true')
+		.addRemote('origin', repository)
+		.fetch()
+		.checkout('origin', 'master');
+};
+
+const start = () => {
+	const express = require('express');
+	const router = require('./routes/router.js');
+	const app = express();
+	const port = process.env.PORT || 8085;
+	const U = require('./helper/utilities.js');
+	(U.exists('github') ? Promise.resolve('Exists') : initRepository())
+		.then((res) => {
+			app.use('/', router); // Register router
+			app.listen(port); // Start server
+		})
+};
+
+start();
