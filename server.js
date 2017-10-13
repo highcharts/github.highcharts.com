1d10729034b657ca8297a1a69a400bed73a6c0ce
jon-a-nygaard <jon.arild@highsoft.com>
2017-10-13 10:02:26 +0200

    Moved contents of server.js to app folder.

--- server.js
+++ server.js
@@ -1,26 +1,2 @@
-/**
- * Server application. 
- * Fires up a server using ExpressJS, and registers routers, and starts listening to a port.
- * All processes related to startup belongs in this script.
- * @author Jon Arild Nygard
- * @todo Add license
- */
-'use strict';
-const express = require('express');
-const router = require('./app/router.js');
-const config = require('./config.json');
-const app = express();
-const port = process.env.PORT || config.port || 80;
-console.log('Listening to port: ' + port);
-app.use((req, res, next) => {
-	req.rawBody = '';
-	req.on('data', chunk => {
-		req.rawBody += chunk;
-	});
-	req.on('end', () => {
-		req.body = req.rawBody !== '' ? JSON.parse(req.rawBody) : {};
-		next();
-	});
-})
-app.use('/', router); // Register router
-app.listen(port); // Start server
+// Start server
+require('./app/server.js')
