4b9589b3313373afa8f7a30a151948ac6d3c6aa7
jon-a-nygaard <jon.arild@highsoft.com>
2017-10-16 19:46:04 +0200

    Fixed issue in server.js, data from request is received as a buffer, not a string.

--- app/server.js
+++ app/server.js
@@ -10,8 +10,7 @@ const express = require('express')
 const router = require('./router.js')
 const config = require('../config.json')
 const {
-  isJSON,
-  isString
+  isJSON
 } = require('./utilities.js')
 const app = express()
 const port = process.env.PORT || config.port || 80
@@ -19,9 +18,8 @@ const port = process.env.PORT || config.port || 80
 const bodyJSONParser = (req, res, next) => {
   req.rawBody = ''
   req.on('data', chunk => {
-    if (isString(chunk)) {
-      req.rawBody += chunk
-    }
+    // chunk is a buffer, but is converted to a string in the assignment.
+    req.rawBody += chunk
   })
   req.on('end', () => {
     if (isJSON(req.rawBody)) {
