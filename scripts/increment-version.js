b58c237476f3b5a3690dc2decfd1942a0a5c4b21
jon-a-nygaard <jon.arild@highsoft.com>
2019-03-04 11:08:48 +0100

    Remove increment version script

--- scripts/increment-version.js
+++
@@ -1,9 +0,0 @@
-let json = require('../package.json')
-const fs = require('fs')
-const delimiter = '.'
-let arr = json.version.split(delimiter)
-let minor = arr.pop()
-minor += 1 // Increment the version number.
-arr.push(minor)
-json.version = arr.join(delimiter)
-fs.writeFileSync('../package.json', json)
