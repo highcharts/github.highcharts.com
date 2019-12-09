a3138643e8004e7f7c5c88ea35daa98c56656232
jon-a-nygaard <jon.arild@highsoft.com>
2019-12-09 09:53:15 +0100

    Refactor app/webhook.js

--- app/webhook.js
+++ app/webhook.js
@@ -1,24 +1,22 @@
 /**
- * Utility functions to validate POST from a Github webhook.
+ * Utility functions to validate a POST from a GitHub webhook.
  * @author Jon Arild Nygard
  * @todo Add license
  */
 'use strict'
+
+// Import dependencies, sorted by path name.
+const { isObject, isString } = require('./utilities.js')
 const crypto = require('crypto')
-const {
-  isObject,
-  isString
-} = require('./utilities.js')
 
 /**
- * sha1 - Create a sha1 string, uses a secret string to cryptate the data.
+ * Creates a sha1 string, uses a secret string to cryptate the data. Returns a
+ * sha1 string, or false if invalid input parameters.
  *
  * @param {string} secret The secret to cryptate with.
  * @param {string} data The data to cryptate.
- * @return {string|false} Returns a sha1 string. Returns false if invalid input
- *     parameters.
  */
-const sha1 = (secret, data) => {
+function sha1 (secret, data) {
   let result = false
   if (isString(secret) && isString(data)) {
     result = crypto.createHmac('sha1', secret)
@@ -29,16 +27,46 @@ const sha1 = (secret, data) => {
 }
 
 /**
+ * Checks if a POST is from a valid Github webhook, that it is secure and has
+ * valid data. Returns an object containing the properties {boolean} valid,
+ * and {string} message.
+ *
+ * @param {Request} [request] An ExpressJS request object.
+ * @param {object} [request.body] An object representation of the request body.
+ * @param {object} [request.headers] An object representation of the request
+ * headers.
+ * @param {string} [request.rawBody] A string representation of the request
+ * body.
+ * @param {string} [secureToken] The secure token used in cryptating the body.
+ */
+function validateWebHook (request, secureToken) {
+  return !(isObject(request) && isString(secureToken))
+    ? { valid: false, message: 'Invalid input parameters' }
+    : !(isObject(request.body) && isString(request.rawBody))
+      ? { valid: false, message: 'Missing payload' }
+      : !(
+        isString(request.headers['x-hub-signature']) &&
+        validSignature(
+          request.headers['x-hub-signature'],
+          request.rawBody,
+          secureToken
+        ))
+        ? { valid: false, message: 'Invalid signature' }
+        : isString(request.body.ref)
+          ? { valid: true, message: '' }
+          : { valid: false, message: 'Missing Git ref' }
+}
+
+/**
  * validSignature - Checks if the signature is valid. The signature should match
- * the resulting sha1 created from the body and secret.
+ * the resulting sha1 created from the body and secret. Returns true if
+ * signature is valid, false if invalid signature, or invalid input parameters.
  *
  * @param {string} signature The signature to validate.
  * @param {string} body The received content of the body.
  * @param {string} secret The secret token used to cryptate the body.
- * @return {boolean} Returns true if signature is valid. Returns false if
- * invalid signature, or invalid input parameters.
  */
-const validSignature = (signature, body, secret) => {
+function validSignature (signature, body, secret) {
   let result = false
   if (isString(signature) && isString(body) && isString(secret)) {
     const sha = sha1(secret, body)
@@ -50,49 +78,7 @@ const validSignature = (signature, body, secret) => {
   return result
 }
 
-/**
- * validateWebHook - Checks if a POST from a Github is safe and has valid data.
- *
- * @param {object} request An ExpressJS request object.
- * @param {object} request.body An object representation of the request body.
- * @param {object} request.headers An object representation of the request headers.
- * @param {string} request.rawBody A string representation of the request body.
- * @param {string} secureToken The secure token used in cryptating the body.
- * @return {object} Returns an object containing the properties {boolean} valid,
- * and {string} message.
- */
-const validateWebHook = (request, secureToken) => {
-  let valid = false
-  let message = ''
-  if (isObject(request) && isString(secureToken)) {
-    const body = request.body
-    const payload = request.rawBody
-    if (isObject(body) && isString(payload)) {
-      const signature = request.headers['x-hub-signature']
-      if (
-        isString(signature) &&
-        validSignature(signature, payload, secureToken)
-      ) {
-        if (isString(body.ref)) {
-          valid = true
-        } else {
-          message = 'Missing Git ref'
-        }
-      } else {
-        message = 'Invalid signature'
-      }
-    } else {
-      message = 'Missing payload'
-    }
-  } else {
-    message = 'Invalid input parameters'
-  }
-  return {
-    valid: valid,
-    message: message
-  }
-}
-
+// Export webhook functionality
 module.exports = {
   sha1,
   validateWebHook,
